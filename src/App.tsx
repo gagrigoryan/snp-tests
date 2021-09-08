c06f28c8786322c9123a61299a8d0da6faea4cba
rimgo <grigoryang085@gmail.com>
2021-09-08 15:37:03 +0300

    feat: add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-    return <div className="App">App created</div>;
+    return (
+        <>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./homePage.module.scss";
+
+const HomePage: React.FC = () => {
+    return <div className={styles.container}>HomePage</div>;
+};
+
+export default HomePage;
