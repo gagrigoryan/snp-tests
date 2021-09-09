3bece4cf2e433b8f73f02b50592a66d77cfd12d0
rimgo <grigoryang085@gmail.com>
2021-09-09 14:10:12 +0300

    feat: add RadioButton

---
+++ src/components/icons/CheckLightIcon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CheckLightIcon: React.FC = () => (
+    <svg width="1rem" height="0.7rem" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.967651 4.90445L5.61276 9.54956L13.6967 1.46564" stroke="#979797" strokeWidth="1.5" />
+    </svg>
+);
+
+export default CheckLightIcon;
---
+++ src/stories/RadioButton.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import RadioButton from "../components/radio-button/RadioButton";
+
+export default {
+    title: "RadioButton/RadioButton",
+} as Meta;
+
+const Template: Story<ComponentProps<typeof RadioButton>> = (args) => <RadioButton {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {
+    label: "Подтверждаю, что у меня нет конфликта интересов.",
+};
