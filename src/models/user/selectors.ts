1ea739149b885d691dcd543487846300a4051232
rimgo <grigoryang085@gmail.com>
2021-09-11 15:50:47 +0300

    feat: add user selectors

--- src/models/user/selectors.ts
+++ src/models/user/selectors.ts
@@ -3,6 +3,12 @@ import { RootState } from "../../index";
 
 const selectSelf = (state: RootState) => state.userStore;
 
+export const userSelector = createSelector(selectSelf, ({ user }) => user);
+
+export const isAdminSelector = createSelector(selectSelf, ({ user }) => user.is_admin);
+
+export const isAuthSelector = createSelector(selectSelf, ({ isAuth }) => isAuth);
+
 export const loginFailedSelector = createSelector(selectSelf, ({ loginFailed }) => loginFailed);
 
 export const registerFailedSelector = createSelector(selectSelf, ({ registerFailed }) => registerFailed);
